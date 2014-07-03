c2d475118225c6ebcd778037f6091c0cf40ff99e
Noel Cower <ncower@gmail.com>
2014-07-03 13:31:26 -0700

    Use geom_util functions in geometry.m.

--- Hydra/geometry.m
+++ Hydra/geometry.m
@@ -1,4 +1,5 @@
 #import "helpers.h"
+#import "geom_util.h"
 
 static hydradoc doc_geometry_intersectionrect = {
     "geometry", "intersectionrect", "geometry.intersectionrect(rect1, rect2) -> rect3",
@@ -6,25 +7,11 @@ static hydradoc doc_geometry_intersectionrect = {
 };
 
 int geometry_intersectionrect(lua_State* L) {
-    CGFloat x1 = (lua_getfield(L, 1, "x"), lua_tonumber(L, -1));
-    CGFloat y1 = (lua_getfield(L, 1, "y"), lua_tonumber(L, -1));
-    CGFloat w1 = (lua_getfield(L, 1, "w"), lua_tonumber(L, -1));
-    CGFloat h1 = (lua_getfield(L, 1, "h"), lua_tonumber(L, -1));
+    NSRect lhs = hydra_to_nsrect(L, 1);
+    NSRect rhs = hydra_to_nsrect(L, 2);
+    NSRect result = NSIntersectionRect(lhs, rhs);
     
-    CGFloat x2 = (lua_getfield(L, 2, "x"), lua_tonumber(L, -1));
-    CGFloat y2 = (lua_getfield(L, 2, "y"), lua_tonumber(L, -1));
-    CGFloat w2 = (lua_getfield(L, 2, "w"), lua_tonumber(L, -1));
-    CGFloat h2 = (lua_getfield(L, 2, "h"), lua_tonumber(L, -1));
-    
-    NSRect r1 = NSMakeRect(x1, y1, w1, h1);
-    NSRect r2 = NSMakeRect(x2, y2, w2, h2);
-    NSRect r3 = NSIntersectionRect(r1, r2);
-    
-    lua_newtable(L);
-    lua_pushnumber(L, r3.origin.x);    lua_setfield(L, -2, "x");
-    lua_pushnumber(L, r3.origin.y);    lua_setfield(L, -2, "y");
-    lua_pushnumber(L, r3.size.width);  lua_setfield(L, -2, "w");
-    lua_pushnumber(L, r3.size.height); lua_setfield(L, -2, "h");
+    hydra_push_nsrect(L, result);
     
     return 1;
 }
